5e8d7faf52b54df42b9c804572fb48e18462870c
alexrmr08 <walter.romero123.2@gmail.com>
2026-05-09 18:41:03 -0500

    doc: Completé la clase adaptador para que todas los interfaces y clases esten listas para el main

--- src/lab05_adaptador/AdaptadorDrone.java
+++ src/lab05_adaptador/AdaptadorDrone.java
@@ -1,7 +1,15 @@
 package lab05_adaptador;
-public class AdaptadorDrone {
+public class AdaptadorDrone implements Pato{
     protected Drone drone;
     public void AdaptadorDrone(Drone drone){
         this.drone= drone;
+    }
+    @Override
+    public void cuaquear(){
+        drone.beep();
+    }
+    @Override
+    public void volar(){
+        drone.despegar();
     }
 }
